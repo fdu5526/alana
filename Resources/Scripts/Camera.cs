2a04d6a3c0b5ee6595061339eb5f4a5ce431885a
Dave Yan <fdu5526@live.com>
2015-10-02 20:31:47 -0700

    starting camera script

--- Resources/Scripts/Camera.cs
+++ Resources/Scripts/Camera.cs
@@ -3,10 +3,35 @@ using System.Collections;
 
 public class Camera : MonoBehaviour {
 
+	Rigidbody2D rb;
+	float speed = 0.1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D>();
+		Invoke("MoveToStairs", 2f);
+		Invoke("MoveUpStairs", 4f);
+		Invoke("MoveToPimp", 	 6f);
+		Invoke("TalkToPimp", 	 8f);
+
 	}
+	
+	void MoveToStairs () {
+		rb.velocity = new Vector2(speed, 0f);
+	}
+
+	void MoveUpStairs () {
+		rb.velocity = new Vector2(speed, speed);
+	}
+
+	void MoveToPimp () {
+		rb.velocity = new Vector2(speed, 0f);
+	}
+
+	void TalkToPimp () {
+		rb.velocity = new Vector2(0f, 0f);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
