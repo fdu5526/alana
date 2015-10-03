61d54ede66d50d21f7c6c614660cf1f166d91224
Dave Yan <fdu5526@live.com>
2015-10-02 22:42:07 -0700

    added alana

--- Resources/Scripts/Camera.cs
+++ Resources/Scripts/Camera.cs
@@ -4,32 +4,52 @@ using System.Collections;
 public class Camera : MonoBehaviour {
 
 	Rigidbody2D rb;
-	float speed = 0.1f;
+	float speed = 0.5f;
+	GameObject alana;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
-		Invoke("MoveToStairs", 2f);
-		Invoke("MoveUpStairs", 4f);
-		Invoke("MoveToPimp", 	 6f);
-		Invoke("TalkToPimp", 	 8f);
+		alana = GameObject.Find("Alana");
 
+
+		float start = 0f;
+
+		Invoke("StartMoving", start);
+		Invoke("MoveToStairs", start);
+		Invoke("MoveUpStairs", start + 13f);
+		Invoke("MoveToPimp", 	 start + 18f);
+		Invoke("TalkToPimp", 	 start + 22f);
+
+	}
+
+	void StartMoving () {
+		Vector2 v = new Vector2(speed, 0f);
+		alana.GetComponent<Rigidbody2D>().velocity = v;
 	}
 	
 	void MoveToStairs () {
-		rb.velocity = new Vector2(speed, 0f);
+		Vector2 v = new Vector2(speed, 0f);
+		rb.velocity = v;
+		alana.GetComponent<Rigidbody2D>().velocity = v;
 	}
 
 	void MoveUpStairs () {
-		rb.velocity = new Vector2(speed, speed);
+		Vector2 v = new Vector2(0.67f, 0.224f);
+		rb.velocity = new Vector2(0.1f, 0.1f);
+		alana.GetComponent<Rigidbody2D>().velocity = v;
 	}
 
 	void MoveToPimp () {
-		rb.velocity = new Vector2(speed, 0f);
+		Vector2 v = new Vector2(speed, 0f);
+		rb.velocity = v;
+		alana.GetComponent<Rigidbody2D>().velocity = v;
 	}
 
 	void TalkToPimp () {
-		rb.velocity = new Vector2(0f, 0f);
+		Vector2 v = new Vector2(0f, 0f);
+		rb.velocity = v;
+		alana.GetComponent<Rigidbody2D>().velocity = v;
 	}
 
 	
