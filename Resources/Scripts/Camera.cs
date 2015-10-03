b6d57d424f02c46d029f0b81947d40f30a5ebbb6
Dave Yan <fdu5526@live.com>
2015-10-03 00:24:04 -0700

    has text and pimp

--- Resources/Scripts/Camera.cs
+++ Resources/Scripts/Camera.cs
@@ -7,18 +7,29 @@ public class Camera : MonoBehaviour {
 	float speed = 0.5f;
 	GameObject alana;
 
+	Dialogue[] dialogues;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		alana = GameObject.Find("Alana");
 
 
-		float start = 4f;
+		int numTexts = 3;
+		dialogues = new Dialogue[numTexts];
+
+		for (int i = 0; i < numTexts; i++) {
+			dialogues[i] = GameObject.Find("Text " + i).GetComponent<Dialogue>();
+		}
+
+
+		float start = 0f;
 
 		Invoke("MoveToStairs", start);
 		Invoke("MoveUpStairs", start + 13f);
 		Invoke("MoveToPimp", 	 start + 18f);
-		Invoke("TalkToPimp", 	 start + 22f);
+		Invoke("TalkToPimp1",  start + 20f);
+		Invoke("TalkToPimp2",  start + 24f);
 
 	}
 
@@ -35,19 +46,28 @@ public class Camera : MonoBehaviour {
 		Vector2 v = new Vector2(0.67f, 0.224f);
 		rb.velocity = new Vector2(0.1f, 0.1f);
 		alana.GetComponent<Rigidbody2D>().velocity = v;
+
+
 	}
 
 	void MoveToPimp () {
 		Vector2 v = new Vector2(speed, 0f);
 		rb.velocity = v;
 		alana.GetComponent<Rigidbody2D>().velocity = v;
+
+		dialogues[0].Display();
 	}
 
-	void TalkToPimp () {
+	void TalkToPimp1 () {
+		dialogues[1].Display();
+	}
+
+	void TalkToPimp2 () {
 		Vector2 v = new Vector2(0f, 0f);
 		rb.velocity = v;
 		alana.GetComponent<Rigidbody2D>().velocity = v;
 		alana.GetComponent<Animator>().SetBool("walk", false);
+		dialogues[2].Display();
 	}
 
 	
---
+++ Resources/Scripts/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+
+	public float duration;
+	public float fadeDuration;
+
+	float startTime;
+	bool started;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<TextMesh>().color = new Color(1f,1f,1f,0f);
+		GetComponent<MeshRenderer>().sortingOrder = 3000;
+		started = false;
+	}
+
+	public void Display () {
+		startTime = Time.time;
+		started = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (started ) { // display full color
+			
+			float d = 0f;
+
+			if (Time.time - startTime < fadeDuration) { // fade in
+				d = (Time.time - startTime) / fadeDuration;
+			}else if (Time.time - startTime < duration + fadeDuration) { // full color
+				d = 1f;
+			} else if (Time.time - startTime < duration + 2f*fadeDuration) { // fade out
+				d = 1f - (Time.time - startTime - fadeDuration - duration) / fadeDuration;
+			} else {
+				started = false;
+			}
+			
+			GetComponent<TextMesh>().color = new Color(1f,1f,1f,d);
+		} 
+	}
+}
