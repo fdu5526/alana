b669d2be66262d30b0213f5b3d5e9c65006ca332
Dave Yan <fdu5526@live.com>
2015-10-03 01:16:08 -0700

    all text of before transition are added

---
+++ Resources/Scripts/BlackFade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlackFade : MonoBehaviour {
+
+
+	public enum FadeState { None, FadeIn, FadeOut };
+
+	public FadeState state;
+	public float rate = 0.01f;
+	
+	float t;
+
+
+	// Use this for initialization
+	void Start () {
+		state = FadeState.FadeOut;
+		t = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+			case FadeState.FadeIn:
+				t = Mathf.Min(1f, t + rate);
+				break;
+			case FadeState.FadeOut:
+				t = Mathf.Max(0f, t - rate);
+				break;
+		}
+
+		GetComponent<Image>().color = new Color(0f, 0f, 0f, t);
+	}
+}
--- Resources/Scripts/Narrative.cs
+++ Resources/Scripts/Narrative.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera : MonoBehaviour {
+public class Narrative : MonoBehaviour {
 
 	Rigidbody2D rb;
 	float speed = 0.5f;
 	GameObject alana;
+	BlackFade fade;
 
 	Dialogue[] dialogues;
 
@@ -13,9 +14,10 @@ public class Camera : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		alana = GameObject.Find("Alana");
+		fade = GameObject.Find("Canvas/Black").GetComponent<BlackFade>();
 
 
-		int numTexts = 7;
+		int numTexts = 11;
 		dialogues = new Dialogue[numTexts];
 
 		for (int i = 0; i < numTexts; i++) {
@@ -27,6 +29,7 @@ public class Camera : MonoBehaviour {
 
 		Invoke("MoveToStairs", start);
 		Invoke("MoveUpStairs", start + 13f);
+		
 		Invoke("MoveToPimp", 	 start + 18f);
 		Invoke("TalkToPimp1",  start + 20f);
 		Invoke("TalkToPimp2",  start + 24f);
@@ -34,6 +37,14 @@ public class Camera : MonoBehaviour {
 		Invoke("TalkToPimp4",  start + 30f);
 		Invoke("TalkToPimp5",  start + 34f);
 		Invoke("TalkToPimp6",  start + 37f);
+		Invoke("TalkToPimp7",  start + 38f);
+		Invoke("TalkToPimp8",  start + 40f);
+		Invoke("TalkToPimp9",  start + 42f);
+		Invoke("TalkToPimp10", start + 44f);
+
+		Invoke("StopCamera", start + 54f);
+		Invoke("StopWalk", start + 63f);
+		Invoke("CutToKnock", start + 67f);
 
 	}
 
@@ -50,8 +61,6 @@ public class Camera : MonoBehaviour {
 		Vector2 v = new Vector2(0.67f, 0.224f);
 		rb.velocity = new Vector2(0.1f, 0.1f);
 		alana.GetComponent<Rigidbody2D>().velocity = v;
-
-
 	}
 
 	void MoveToPimp () {
@@ -88,6 +97,41 @@ public class Camera : MonoBehaviour {
 
 	void TalkToPimp6 () {
 		dialogues[6].Display(); // Don't mess up this time.
+	}
+
+	void TalkToPimp7 () {
+		dialogues[7].Display(); // I'll do my best.
+	}
+
+	void TalkToPimp8 () {
+		dialogues[8].Display(); // Alright.
+	}
+
+	void TalkToPimp9 () {
+		dialogues[9].Display(); // Go.
+	}
+
+	void TalkToPimp10 () {
+		Vector2 v = new Vector2(speed, 0f);
+		rb.velocity = v;
+		alana.GetComponent<Rigidbody2D>().velocity = v;
+		alana.GetComponent<Animator>().SetBool("walk", true);
+		dialogues[10].Display(); // You got this.
+	}
+
+	void StopCamera () {
+		rb.velocity = Vector2.zero;
+	}
+
+	void StopWalk () {
+		alana.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		alana.GetComponent<Animator>().SetBool("walk", false);
+	}
+
+
+	void CutToKnock () {
+		fade.rate = 0.1f;
+		fade.state = BlackFade.FadeState.FadeIn;
 	}
 
 	
